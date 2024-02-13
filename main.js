9efcac3c526103b99317c976d45f518a3c526a4e
Samuel Nussbaumer <samuel.nussbaumer@stud.hslu.ch>
2024-02-13 17:12:58 +0100

    initialized services snoozebaumer/wato#1

---
+++ main.js
@@ -0,0 +1,33 @@
+const bodyParser = require("body-parser");
+const express = require("express");
+const server = express();
+
+server.use(bodyParser.json());
+
+server.post('/game', async (req, res) => {
+    console.log(req.body);
+    res.send("Game posted");
+});
+
+server.get('/game/:id', async(req, res) => {
+    console.log(req.params.id);
+
+    const result = "Post with id " + req.params.id + " found.";
+    if (result) {
+        res.send(result);
+    } else {
+        res.status(404);
+    }
+
+    res.end();
+});
+
+server.put("/game/:id", async (req, res) => {
+    console.log(req.params.id);
+
+
+    res.status(200);
+    res.end();
+});
+
+server.listen(4566);
