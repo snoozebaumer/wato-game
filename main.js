889ff402f0aa20e05f27d345a8c70f3dd08c3843
Samuel Nussbaumer <samuel.nussbaumer@stud.hslu.ch>
2024-02-16 19:26:49 +0100

    mongodb connection, create object and return id snoozebaumer/wato#1

--- main.js
+++ main.js
@@ -1,14 +1,38 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const server = express();
+const {MongoClient, ServerApiVersion, ObjectId} = require("mongodb");
 require('dotenv').config();
+
+const server = express();
+const port = 4566;
 
 
 server.use(bodyParser.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
 server.post('/game', async (req, res) => {
-    console.log(req.body);
-    res.send("Game posted");
+    const {challenger, challenge} = req.body;
+    try {
+        await client.connect();
+        const db = await client.db(process.env.DB_NAME);
+        const id = (await db.collection("challenge").insertOne({challenger: challenger, description: challenge})).insertedId;
+        res.send({"id": id.toString()});
+    }   catch (e) {
+        res.status(500).send(e);
+    }
+    finally {
+        await client.close();
+        res.end();
+    }
 });
 
 server.get('/game/:id', async(req, res) => {
@@ -32,4 +56,6 @@ server.put("/game/:id", async (req, res) => {
     res.end();
 });
 
-server.listen(4566);
+server.listen(port, () => {
+    console.log("GAME: listening on port ", port);
+});
